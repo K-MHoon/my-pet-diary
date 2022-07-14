72a527f9ad450c0caa2996d3f3613751ac6c80f1
K-MHOON <azmria117@gmail.com>
2022-07-14 21:40:15 +0900

    Initial commit

---
+++ src/test/java/com/kmhoon/mypetdiary/MyPetDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmhoon.mypetdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPetDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

92a668c0c8541e9163575fb5629ec93f75ef5896
K-MHOON <azmria117@gmail.com>
2023-09-05 21:33:08 +0900

    fix : common 모듈 의존성 추가 및 컴포넌트 스캔 경로 추가

--- service/src/main/java/com/kmhoon/service/DiaryServiceApplication.java
+++ service/src/main/java/com/kmhoon/service/DiaryServiceApplication.java
@@ -2,8 +2,10 @@ package com.kmhoon.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.kmhoon")
 public class DiaryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(DiaryServiceApplication.class, args);

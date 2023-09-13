963d8396c43a6e62d67b14ce3626ed7ed617b26f
K-MHOON <azmria117@gmail.com>
2023-09-13 21:48:42 +0900

    test : 반려동물 목록 확인 통합 테스트 추가

---
+++ service/src/test/java/com/kmhoon/service/service/pet/PetServiceTest.java
@@ -0,0 +1,76 @@
+package com.kmhoon.service.service.pet;
+
+import com.kmhoon.common.enums.Gender;
+import com.kmhoon.common.model.entity.Owner;
+import com.kmhoon.common.model.entity.Pet;
+import com.kmhoon.common.repository.OwnerRepository;
+import com.kmhoon.common.repository.PetRepository;
+import com.kmhoon.service.service.ServiceIntegrationTestBase;
+import com.kmhoon.service.service.pet.response.PetServiceResponse;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+class PetServiceTest extends ServiceIntegrationTestBase {
+
+    @Autowired
+    OwnerRepository ownerRepository;
+
+    @Autowired
+    PetRepository petRepository;
+
+    @Autowired
+    PetService petService;
+    @Test
+    @DisplayName("반려동물 목록을 확인한다.")
+    void getPetList() {
+        // given
+        Owner owner = Owner.builder()
+                .nickName("testUser")
+                .gender(Gender.MAN)
+                .email("test@test.com")
+                .build();
+        Owner savedOwner = ownerRepository.save(owner);
+
+        Pet pet1 = Pet.builder()
+                .age(1L)
+                .name("테스트펫1")
+                .gender(Gender.MALE)
+                .weight(5.1)
+                .species("시바견")
+                .registeredNumber("testRegisteredNumber")
+                .live(Boolean.TRUE)
+                .adoptedDate(LocalDateTime.of(2023, 9, 11, 0, 0, 0))
+                .owner(savedOwner)
+                .build();
+
+        Pet pet2 = Pet.builder()
+                .age(1L)
+                .name("테스트펫2")
+                .gender(Gender.FEMALE)
+                .weight(6.2)
+                .species("시바견")
+                .registeredNumber("testRegisteredNumber2")
+                .live(Boolean.TRUE)
+                .adoptedDate(LocalDateTime.of(2023, 9, 10, 0, 0, 0))
+                .owner(savedOwner)
+                .build();
+
+        petRepository.save(pet1);
+        petRepository.save(pet2);
+
+        // when
+        PetServiceResponse.GetPetList result = petService.getPetList(savedOwner.getId());
+
+        // then
+        assertThat(result.getPetList()).hasSize(2)
+                .extracting("name", "weight", "species", "registeredNumber")
+                .contains(tuple("테스트펫1", 5.1, "시바견", "testRegisteredNumber"),
+                        tuple("테스트펫2", 6.2, "시바견", "testRegisteredNumber2"));
+    }
+}

951730450c10e5467373af005268574258bdd794
K-MHOON <azmria117@gmail.com>
2023-09-21 00:24:41 +0900

    feat : Jwt Key, Time 설정 추가

---
+++ service/src/main/java/com/kmhoon/service/security/JwtKey.java
@@ -0,0 +1,21 @@
+package com.kmhoon.service.security;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+@Getter
+public class JwtKey {
+
+    private Key key;
+
+    public JwtKey(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+}
---
+++ service/src/main/java/com/kmhoon/service/security/JwtTime.java
@@ -0,0 +1,16 @@
+package com.kmhoon.service.security;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class JwtTime {
+
+    @Value("${jwt.time.access}")
+    private Long accessTokenTime;
+
+    @Value("${jwt.time.refresh}")
+    private Long refreshTokenTime;
+}

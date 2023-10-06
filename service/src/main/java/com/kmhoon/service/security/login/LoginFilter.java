b8b30205202370639d0911d59b1fb622a90b4554
K-MHOON <azmria117@gmail.com>
2023-10-06 21:20:21 +0900

    feat : 로그인 필터, 성공, 실패 Handler 추가

---
+++ service/src/main/java/com/kmhoon/service/security/login/LoginFilter.java
@@ -0,0 +1,21 @@
+package com.kmhoon.service.security.login;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        return super.requiresAuthentication(request, response);
+    }
+}

3caaa4de4215aa8a2e21310f17ad835039404859
K-MHOON <azmria117@gmail.com>
2023-10-15 21:54:11 +0900

    feat : Refrigerator RefrigeratorItem 연관관계 추가

--- common/src/main/java/com/kmhoon/common/model/entity/Refrigerator.java
+++ common/src/main/java/com/kmhoon/common/model/entity/Refrigerator.java
@@ -3,14 +3,16 @@ package com.kmhoon.common.model.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 반려동물이 먹은 사료/간식 저장소
  */
 @Entity
 @Table(name = "tb_refrigerator")
-@AllArgsConstructor
-@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @ToString(exclude = {"pet"})
 @Getter
@@ -22,6 +24,10 @@ public class Refrigerator extends BaseEntity{
 
     @OneToOne(mappedBy = "refrigerator", fetch = FetchType.LAZY)
     private Pet pet;
+
+    @OneToMany(mappedBy = "refrigerator")
+    @Builder.Default
+    private List<RefrigeratorItem> refrigeratorItemList = new ArrayList<>();
 
     // TODO Food 연관 관계, 간식, 사료는 타입에 따라 분류
 }

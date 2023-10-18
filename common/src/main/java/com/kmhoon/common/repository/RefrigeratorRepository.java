3d1026519e856046917fad1d3b2b6859d8b6e36b
K-MHOON <azmria117@gmail.com>
2023-10-18 21:28:10 +0900

    feat : petId를 바탕으로 냉장고 정보 조회하는 API 추가

--- common/src/main/java/com/kmhoon/common/repository/RefrigeratorRepository.java
+++ common/src/main/java/com/kmhoon/common/repository/RefrigeratorRepository.java
@@ -1,7 +1,17 @@
 package com.kmhoon.common.repository;
 
+import com.kmhoon.common.model.entity.Pet;
 import com.kmhoon.common.model.entity.Refrigerator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface RefrigeratorRepository extends JpaRepository<Refrigerator, Long> {
+
+    @Query("select distinct r from Refrigerator r " +
+            "join fetch r.refrigeratorItemList " +
+            "where r.pet = :pet")
+    Optional<Refrigerator> findByPet(@Param("pet") Pet pet);
 }

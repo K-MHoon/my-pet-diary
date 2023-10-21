f6a9810313883def7c560471b357a21dfb6cdf58
K-MHOON <azmria117@gmail.com>
2023-10-21 19:49:55 +0900

    feat : addRefrigeratorItem 기능 추가, @Transactional 누락 추가

---
+++ common/src/main/java/com/kmhoon/common/repository/RefrigeratorItemRepository.java
@@ -0,0 +1,7 @@
+package com.kmhoon.common.repository;
+
+import com.kmhoon.common.model.entity.RefrigeratorItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefrigeratorItemRepository extends JpaRepository<RefrigeratorItem, Long> {
+}
--- service/src/main/java/com/kmhoon/service/service/refrigerator/RefrigeratorService.java
+++ service/src/main/java/com/kmhoon/service/service/refrigerator/RefrigeratorService.java
@@ -2,6 +2,8 @@ package com.kmhoon.service.service.refrigerator;
 
 import com.kmhoon.common.model.entity.Pet;
 import com.kmhoon.common.model.entity.Refrigerator;
+import com.kmhoon.common.model.entity.RefrigeratorItem;
+import com.kmhoon.common.repository.RefrigeratorItemRepository;
 import com.kmhoon.common.repository.RefrigeratorRepository;
 import com.kmhoon.service.exception.DiaryServiceException;
 import com.kmhoon.service.exception.enums.entity.refrigerator.RefrigeratorExceptionCode;
@@ -10,6 +12,7 @@ import com.kmhoon.service.service.refrigerator.response.RefrigeratorServiceRespo
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
@@ -18,12 +21,25 @@ public class RefrigeratorService {
 
     private final PetCommonService petCommonService;
     private final RefrigeratorRepository refrigeratorRepository;
+    private final RefrigeratorItemRepository refrigeratorItemRepository;
 
+    @Transactional(readOnly = true)
     public RefrigeratorServiceResponse.GetRefrigeratorDetail getRefrigeratorDetail(final Long petId) {
         Pet pet = petCommonService.getPetBy(petId);
         Refrigerator refrigerator = refrigeratorRepository.findByPet(pet).orElseThrow(() -> new DiaryServiceException(RefrigeratorExceptionCode.REFRIGERATOR_NOT_FOUND));
         return RefrigeratorServiceResponse.GetRefrigeratorDetail.of(refrigerator);
     }
 
-
+    @Transactional
+    public void addRefrigeratorItem(final Long petId, RefrigeratorServiceResponse.RefrigeratorItemDto request) {
+        Pet pet = petCommonService.getPetBy(petId);
+        Refrigerator refrigerator = pet.getRefrigerator();
+        RefrigeratorItem refrigeratorItem = RefrigeratorItem.builder()
+                .information(request.getInformation())
+                .name(request.getName())
+                .itemType(request.getItemType())
+                .refrigerator(refrigerator)
+                .build();
+        refrigeratorItemRepository.save(refrigeratorItem);
+    }
 }

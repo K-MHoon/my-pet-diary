4c35f1de717acac62fa3679e827624ae9f6fa057
K-MHOON <azmria117@gmail.com>
2023-10-24 20:03:16 +0900

    feat : addRefrigerator API 추가

--- service/src/main/java/com/kmhoon/service/controller/refrigerator/RefrigeratorController.java
+++ service/src/main/java/com/kmhoon/service/controller/refrigerator/RefrigeratorController.java
@@ -1,11 +1,14 @@
 package com.kmhoon.service.controller.refrigerator;
 
+import com.kmhoon.service.controller.refrigerator.request.RefrigeratorControllerRequest;
 import com.kmhoon.service.service.refrigerator.RefrigeratorService;
 import com.kmhoon.service.service.refrigerator.response.RefrigeratorServiceResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @Slf4j
@@ -19,5 +22,11 @@ public class RefrigeratorController {
     @ResponseStatus(HttpStatus.OK)
     public RefrigeratorServiceResponse.GetRefrigeratorDetail getRefrigeratorDetail(@PathVariable Long petId) {
         return service.getRefrigeratorDetail(petId);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
+    public void addRefrigeratorItem(@PathVariable Long petId, @RequestBody @Valid RefrigeratorControllerRequest.AddRefrigeratorItem request) {
+        service.addRefrigeratorItem(petId, request.toServiceRequest());
     }
 }
---
+++ service/src/main/java/com/kmhoon/service/controller/refrigerator/request/RefrigeratorControllerRequest.java
@@ -0,0 +1,36 @@
+package com.kmhoon.service.controller.refrigerator.request;
+
+import com.kmhoon.common.enums.ItemType;
+import com.kmhoon.service.service.refrigerator.response.RefrigeratorServiceResponse;
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RefrigeratorControllerRequest {
+
+    @Getter
+    @Builder
+    @NoArgsConstructor(access = AccessLevel.PRIVATE)
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    public static final class AddRefrigeratorItem {
+
+        @NotNull
+        private ItemType itemType;
+
+        @NotBlank
+        private String name;
+
+        @NotBlank
+        private String information;
+
+        public RefrigeratorServiceResponse.RefrigeratorItemDto toServiceRequest() {
+            return RefrigeratorServiceResponse.RefrigeratorItemDto.builder()
+                    .itemType(this.itemType)
+                    .name(this.name)
+                    .information(this.information)
+                    .build();
+        }
+    }
+}

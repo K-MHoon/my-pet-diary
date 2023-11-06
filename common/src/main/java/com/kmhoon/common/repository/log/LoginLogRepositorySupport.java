5176e2e19ca3552b227cd53f1e527956aa1aaa95
K-MHOON <azmria117@gmail.com>
2023-11-06 21:01:40 +0900

    feat : LoginLog 목록 조회 Querydsl 작성

--- common/src/main/java/com/kmhoon/common/repository/log/LoginLogRepositorySupport.java
+++ common/src/main/java/com/kmhoon/common/repository/log/LoginLogRepositorySupport.java
@@ -1,4 +1,12 @@
 package com.kmhoon.common.repository.log;
 
+import com.kmhoon.common.enums.LoginStatus;
+import com.kmhoon.common.model.entity.LoginLog;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface LoginLogRepositorySupport {
+
+    List<LoginLog> findAllByEmailAndLoginStatusAndDateBetween(String email, LoginStatus loginStatus, LocalDate startDate, LocalDate endDate);
 }
--- common/src/main/java/com/kmhoon/common/repository/log/LoginLogRepositorySupportImpl.java
+++ common/src/main/java/com/kmhoon/common/repository/log/LoginLogRepositorySupportImpl.java
@@ -1,11 +1,46 @@
 package com.kmhoon.common.repository.log;
 
+import com.kmhoon.common.enums.LoginStatus;
 import com.kmhoon.common.model.entity.LoginLog;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Objects;
+
+import static com.kmhoon.common.model.entity.QLoginLog.loginLog;
 
 public class LoginLogRepositorySupportImpl extends QuerydslRepositorySupport implements LoginLogRepositorySupport {
 
     public LoginLogRepositorySupportImpl() {
         super(LoginLog.class);
+    }
+
+    @Override
+    public List<LoginLog> findAllByEmailAndLoginStatusAndDateBetween(String email, LoginStatus loginStatus, LocalDate startDate, LocalDate endDate) {
+        return from(loginLog)
+                .where(searchByEmail(email),
+                        searchByLoginStatus(loginStatus),
+                        searchDateBetweenBy(startDate, endDate))
+                .fetch();
+    }
+
+    private BooleanExpression searchDateBetweenBy(LocalDate startDate, LocalDate endDate) {
+        if(Objects.isNull(startDate) || Objects.isNull(endDate)) return null;
+        return loginLog.createdAt.between(LocalDateTime.of(startDate, LocalTime.MIN), LocalDateTime.of(endDate, LocalTime.MAX));
+    }
+
+    private BooleanExpression searchByLoginStatus(LoginStatus loginStatus) {
+        if(Objects.isNull(loginStatus)) return null;
+        return loginLog.loginStatus.eq(loginStatus);
+    }
+
+    private BooleanExpression searchByEmail(String email) {
+        if(!StringUtils.hasText(email)) return null;
+        return loginLog.loginEmail.eq(email);
     }
 }

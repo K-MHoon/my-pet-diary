0bff1df4a2d5dbdb8a8cffa5b2d4234a01528179
K-MHOON <azmria117@gmail.com>
2023-11-11 18:19:29 +0900

    feat : BaseEntity 생성자(creator), 수정자(updater) 정보 추가

--- common/src/main/java/com/kmhoon/common/model/entity/BaseEntity.java
+++ common/src/main/java/com/kmhoon/common/model/entity/BaseEntity.java
@@ -1,7 +1,9 @@
 package com.kmhoon.common.model.entity;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -14,6 +16,12 @@ import java.time.LocalDateTime;
 @EntityListeners(value = {AuditingEntityListener.class})
 @Getter
 public abstract class BaseEntity {
+
+    @CreatedBy
+    private String creator;
+
+    @LastModifiedBy
+    private String updater;
 
     @CreatedDate
     @Column(updatable = false)
---
+++ service/src/main/java/com/kmhoon/service/config/AuditorAwareConfig.java
@@ -0,0 +1,24 @@
+package com.kmhoon.service.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Component
+public class AuditorAwareConfig implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(authentication.getName());
+    }
+}
